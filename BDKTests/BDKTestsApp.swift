cc328136f66557f9d13f57a43cc62d17bbd88bf9
setavenger <setor.blagogee@gmx.de>
2023-07-10 15:26:56 +0200

    Initial Commit

---
+++ BDKTests/BDKTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BDKTestsApp.swift
+//  BDKTests
+//
+//  Created by Setor Blagogee on 10.07.23.
+//
+
+import SwiftUI
+
+@main
+struct BDKTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
